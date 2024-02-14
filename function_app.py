6e9bc8449810a9fdd86f7313abec3ca0c7716b4b
Dror Atariah <drorata@gmail.com>
2024-02-14 18:00:38 +0100

    Added ENV var support using pydantic-settings

--- function_app.py
+++ function_app.py
@@ -3,6 +3,15 @@ from datetime import datetime
 import azure.functions as func
 import loguru
 from loguru import logger
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    foo_bar: str
+
+
+settings = Settings()
 
 app = func.FunctionApp()
 
@@ -15,6 +24,7 @@ app = func.FunctionApp()
 )
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     logger.info(f"Loguru version is {loguru.__version__}")
+    logger.info(f"The value of FOO_BAR is: {settings.foo_bar}")
 
     if myTimer.past_due:
         logger.info("The timer is past due!")
